d6817c64c6bd30be8848c7e79b3079c56d333c85
gucio321 <73652197+gucio321@users.noreply.github.com>
2024-04-20 17:57:21 +0200

    cmd: update

--- cmd/yamler/main.go
+++ cmd/yamler/main.go
@@ -12,7 +12,21 @@ func main() {
 	w := &workflow.Workflow{}
 	w.On.PageBuild.EnableEmpty = workflow.FieldOff
 	w.Jobs = make(map[string]*workflow.Job)
-	w.Jobs["tes"] = &workflow.Job{}
+	w.Jobs["tes"] = &workflow.Job{
+		Steps: []*workflow.Step{
+			{
+				Name: "step1",
+				Uses: "actions/checkout@v2",
+			},
+			{
+				Name: "step2",
+				Uses: "actions/setup-node@v2",
+				With: map[string]string{
+					"node-version": "14",
+				},
+			},
+		},
+	}
 	fmt.Println(w.Marshal())
 	wnd := giu.NewMasterWindow("Yamler", 640, 480, 0)
 	wnd.Run(func() {
